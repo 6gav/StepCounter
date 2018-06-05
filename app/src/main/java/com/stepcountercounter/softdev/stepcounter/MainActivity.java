0bc3b9c959bdeb3c6ddcfa746d985e4772418957
Gavin watson <gavset99@gmail.com>
2018-06-05 15:50:50 -0400

    NavBar Screen Added

--- app/src/main/java/com/stepcountercounter/softdev/stepcounter/MainActivity.java
+++ app/src/main/java/com/stepcountercounter/softdev/stepcounter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.stepcountercounter.softdev.stepcounter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View v){
+        Intent newActivity = new Intent(this, MainMenuActivity.class);
+        startActivity(newActivity);
     }
 }
