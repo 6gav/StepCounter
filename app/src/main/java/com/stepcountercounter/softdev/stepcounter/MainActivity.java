2f8cec68e985ca347e5ec71cfd11329d02e2abf5
Gavin watson <gavset99@gmail.com>
2018-06-05 18:32:03 -0400

    StepTracker somewhat implemented

--- app/src/main/java/com/stepcountercounter/softdev/stepcounter/MainActivity.java
+++ app/src/main/java/com/stepcountercounter/softdev/stepcounter/MainActivity.java
@@ -1,20 +1,77 @@
 package com.stepcountercounter.softdev.stepcounter;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        count = (TextView)findViewById(R.id.countTextView);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+
+
+
+
     }
+
+    protected void onResume(){
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null){
+            sensorManager.registerListener(this, countSensor, sensorManager.SENSOR_DELAY_UI);
+
+        }
+        else
+        {
+            Toast.makeText(this, "Count sensor not available", Toast.LENGTH_LONG).show();;
+        }
+    }
+
+    public void onPause(){
+        super.onPause();
+        activityRunning = false;
+    }
+
+
 
     public void sendMessage(View v){
         Intent newActivity = new Intent(this, MainMenuActivity.class);
         startActivity(newActivity);
+
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(activityRunning){
+            count.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
---
+++ app/src/main/java/com/stepcountercounter/softdev/stepcounter/SensorsClient.java
@@ -0,0 +1,16 @@
+package com.stepcountercounter.softdev.stepcounter;
+
+import android.content.Context;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.fitness.FitnessOptions;
+
+public class SensorsClient extends GoogleApi<FitnessOptions> {
+    protected SensorsClient(@NonNull Context context, Api<FitnessOptions> api, Looper looper) {
+        super(context, api, looper);
+    }
+
+}
