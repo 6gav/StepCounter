34bda4e8ebf0c0bed7db3aacb6abbb97f12d7078
Gavin watson <gavset99@gmail.com>
2018-06-07 16:18:41 -0400

    Debug Mode Added - No Functionality yet

--- app/src/main/java/com/stepcountercounter/softdev/stepcounter/MainActivity.java
+++ app/src/main/java/com/stepcountercounter/softdev/stepcounter/MainActivity.java
@@ -9,6 +9,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,22 +17,55 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     private SensorManager sensorManager;
-    private TextView count;
-    boolean activityRunning;
+    private TextView count, count2, count3;
+    private int StepCounts, TapCount;
+    boolean activityRunning, DebugMode;
+    private Button debugStepButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TapCount = 4;
+        DebugMode = false;
         setContentView(R.layout.activity_main);
+        debugStepButton = (Button)findViewById(R.id.debugAddStepsButton);
 
         count = (TextView)findViewById(R.id.countTextView);
+
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
 
-
-
-
-
     }
+
+    private void DebugEnable(){
+        debugStepButton.setVisibility(View.VISIBLE);
+        debugStepButton.setClickable(true);
+        debugStepButton.setEnabled(true);
+    }
+
+    public void DebugClicker(View v){
+        if(TapCount > 0) {
+
+
+
+            if (TapCount <= 4) {
+                String tapText = "You are " + TapCount + " taps away from debug mode.";
+                Toast.makeText(this, tapText, Toast.LENGTH_SHORT).show();
+            }
+            TapCount--;
+        }
+        else if(TapCount == 0)
+        {
+            DebugMode = true;
+            DebugEnable();
+            Toast.makeText(this, "You are now in debug mode.", Toast.LENGTH_SHORT).show();
+            TapCount--;
+        }
+        else
+        {
+
+        }
+    }
+
 
     protected void onResume(){
         super.onResume();
@@ -66,7 +100,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
 
         if(activityRunning){
-            count.setText(String.valueOf(event.values[0]));
+            String tempText = "Steps: " + String.valueOf(event.values[0]);
+            count.setText(tempText);
         }
     }
 
