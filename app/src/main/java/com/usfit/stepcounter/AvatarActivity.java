b3c8d692f1ceee00125b5902502608738abd7662
CrimDee <mcsabi3@gmail.com>
2018-07-26 17:21:20 -0400

    removed needless arrays from avatar activity

--- app/src/main/java/com/usfit/stepcounter/AvatarActivity.java
+++ app/src/main/java/com/usfit/stepcounter/AvatarActivity.java
@@ -22,53 +22,6 @@ public class AvatarActivity extends AppCompatActivity {
     ImageView ivTop,ivBottom,ivFeet;
     SharedPreferences preferences;
 
-    //region arrays
-    int[] top = {
-            R.drawable.outfit_t00,
-            R.drawable.outfit_t01,
-            R.drawable.outfit_t02,
-
-            R.drawable.outfit_t03,
-            R.drawable.outfit_t04,
-            R.drawable.outfit_t05,
-
-            R.drawable.outfit_t06,
-            R.drawable.outfit_t07,
-            R.drawable.outfit_t08,
-
-            R.drawable.outfit_t09,
-            R.drawable.outfit_t10,
-            R.drawable.outfit_t11,
-
-            R.drawable.outfit_t12,
-            R.drawable.outfit_t13,
-            R.drawable.outfit_t14,
-
-            R.drawable.outfit_t15,
-            R.drawable.outfit_t16,
-            R.drawable.outfit_t17,
-    };
-
-    int[] bottom = {
-            R.drawable.outfit_b00,
-            R.drawable.outfit_b01,
-            R.drawable.outfit_b02,
-            R.drawable.outfit_b03,
-            R.drawable.outfit_b04,
-            R.drawable.outfit_b05,
-            R.drawable.outfit_b06,
-            R.drawable.outfit_b07,
-            R.drawable.outfit_b08,
-            R.drawable.outfit_b09,
-    };
-
-    int[] footwear = {
-            R.drawable.outfit_f1,
-            R.drawable.outfit_f1,
-            R.drawable.outfit_f1
-    };
-    //endregion
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
